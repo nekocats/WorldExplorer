a906ea63dc9e7c10a713d0204fc3641c841c98fc
carlostuuling <71810387+carlostuuling@users.noreply.github.com>
2023-04-11 23:12:37 +0300

    changed stuff

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -11,20 +11,18 @@ defineProps({
 
 <template class="bg">
     <Head title="Welcome" />
-    <div class="relative sm:flex  sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
+    <div class="relative flex grid justify-items-center items-center min-h-screen bg-dots-darker bg-center bg-orange-300 selection:bg-red-500 selection:text-white">
+        <div v-if="canLogin" class="w-full rounded-b-[32px] bg-orange-200 sm:fixed  sm:top-0 sm:right-0 p-6 text-right">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold text-orange-900  dark:text-orange-900 text-[40px]">Dashboard</Link>
 
             <template v-else>
-                <Link :href="route('login')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</Link>
+                <Link :href="route('login')" class="font-semibold text-orange-900 text-[40px]  dark:text-orange-900">Log in</Link>
 
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</Link>
+                <Link v-if="canRegister" :href="route('register')" class="ml-4 font-semibold  text-[40px] text-orange-900  dark:text-orange-900 ">Register</Link>
             </template>
         </div>
-        <div class="">
-            <button class="hover:blue-700 text-6xl text-cyan-700 font-bold py-2 px-4">Play</button> <br>
-            <button class="hover:blue-700 text-6xl text-cyan-700 font-bold py-2 px-4">Options</button> <br>
-            <button class="hover:blue-700 text-6xl text-cyan-700 font-bold py-2 px-4">Exit</button> <br>
+        <div>
+            <button class="bg-orange-200 shadow-inner text-7xl text-orange-900 shadow-xl font-bold py-2 px-60 rounded-full">Play</button> <br>
         </div>
     </div>
 </template>
