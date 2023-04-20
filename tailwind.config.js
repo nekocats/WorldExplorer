c8eb4da1a0efa377e7aba010de098d1c3d16674a
Flow <mynameismgamer@gmail.com>
2023-04-20 09:44:09 +0300

    added screen resolutions

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
         './resources/js/**/*.vue',
     ],
 
+    screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+    
     theme: {
         extend: {
             fontFamily: {
