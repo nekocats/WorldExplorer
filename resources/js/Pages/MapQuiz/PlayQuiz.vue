bd297918bf42b7c4f2f3e2e6821aea01e2c0f604
Kaarel Kallas <kaarel.kallas@ametikool.ee>
2023-04-21 11:44:40 +0300

    test

--- resources/js/Pages/MapQuiz/PlayQuiz.vue
+++ resources/js/Pages/MapQuiz/PlayQuiz.vue
@@ -64,8 +64,8 @@ if (gameOver == false) {
     <Head title="Map Quiz"/>
   <div class='relative  bg-frontPageBg bg-cover grid justify-items-center items-center min-h-screen bg-dots-darker bg-center bg-materialgreenbg selection:bg-lime-950 selection:text-white'>
     <div>
-        <h1 class="p-2 text-shadow shadow-black font-raleway text-5xl text-white py-2 px-20 rounded-lg font-bold">{{ questions[currentQ.current].question }}</h1>
-        <h1 class="p-2 text-shadow shadow-black font-raleway text-6xl text-lime-700 py-2 px-20 rounded-lg font-bold">Score: {{ score }}</h1>
+        <h1 class="text-shadow shadow-black font-raleway text-5xl text-white py-2 px-20 rounded-lg font-bold">{{ questions[currentQ.current].question }}</h1>
+        <h1 class="text-shadow shadow-black font-raleway text-6xl text-lime-700 py-2 px-20 rounded-lg font-bold">Score: {{ score }}</h1>
     </div>
     <GuestLayout>
     <GMapMap id="vue-map" ref="myMapRef" :center="center" :zoom="10" map-type-id="terrain" style="width: 100vw; height: 45rem" @click="mark">
