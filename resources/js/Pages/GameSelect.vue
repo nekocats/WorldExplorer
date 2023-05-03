2c929d9ff739a7d197b45500f265aae012be0fb0
Owarice <carlos.tuuling22@gmail.com>
2023-05-03 09:41:33 +0300

    gapbuttons

--- resources/js/Pages/GameSelect.vue
+++ resources/js/Pages/GameSelect.vue
@@ -30,7 +30,7 @@
                 Gamemode
             </h1>
         </div>
-        <div class="grid justify-items-center">
+        <div class="grid justify-items-center gap-6">
         <div>
             <Link :href="route('quiz')" class="rounded-md bg-lime-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Quiz (Work in progress)</Link> <br>
         </div>
