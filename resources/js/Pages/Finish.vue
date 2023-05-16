df591e03ff102355d7998215176910adb5c6cb61
carlostuuling <71810387+carlostuuling@users.noreply.github.com>
2023-05-16 10:13:04 +0300

    ui changes

--- resources/js/Pages/Finish.vue
+++ resources/js/Pages/Finish.vue
@@ -1,6 +1,13 @@
-<script template> 
+<script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 
+    defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
 </script>
 <template>
     <Head title="Finish" />
@@ -9,7 +16,11 @@ import { Head, Link } from '@inertiajs/vue3';
     <h1 class="scrollbar-none text-shadow shadow-black font-raleway text-6xl text-white py-2 px-20 rounded-lg font-bold">Finished!</h1>
     <h1 class="scrollbar-none text-shadow shadow-black font-raleway text-6xl text-lime-700 py-2 px-20 rounded-lg font-bold">Score: {{ score }}</h1>
     </div>
-   <a href="/"><button class=" backdrop-blur-3xl bg-opacity-25 text-shadow shadow-black font-raleway text-5xl text-white py-2 px-20 rounded-lg font-bold">home</button></a>
+            <Link class="hover:shadow-lime-700 hover:border-lime-700 rounded-lg text-shadow shadow-black  text-5xl text-lime-700 hover:animate-pulse transition-all hover:transition-all duration-1000 hover:duration-1000" data-te-toggle="tooltip" title="Go back to the homepage" :href="route('welcome')">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-12 h-12 self-center cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
+            </Link> <br>
    
 </div>
 
