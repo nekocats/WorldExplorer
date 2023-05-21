f848a20cf8b6a231fa6384b031238a888927e853
Owarice <carlos.tuuling22@gmail.com>
2023-05-21 23:14:01 +0300

    fixed score sorting issues

--- resources/js/Pages/Ranking.vue
+++ resources/js/Pages/Ranking.vue
@@ -25,10 +25,13 @@ const fetchScores = async () => {
       if (userScores.length > 0) {
         const highestScore = Math.max(...userScores.map((score) => score.score));
         user.score = highestScore.toFixed(0);
+      } else {
+        user.score = 0; // Set a default score if user has no scores
       }
     });
 
-    users.value.sort((a, b) => b.score - a.score);
+    users.value.sort((a, b) => b.score - a.score); // Sort users based on scores
+
   } catch (error) {
     console.error(error);
   }
@@ -44,17 +47,16 @@ const getUserImageUrl = (imagePath) => {
 </script>
 
 <template #header>
-<AppLayout title="Ranking">
-
-  <div class="relative  bg-materialgreenlight dark:bg-materialgreenbg bg-cover grid pl-20 pr-20 min-h-screen bg-center selection:bg-lime-950 selection:text-white">
+  <AppLayout title="Ranking">
+    <div class="relative bg-materialgreenlight dark:bg-materialgreenbg bg-cover grid pl-20 pr-20 min-h-screen bg-center selection:bg-lime-950 selection:text-white">
       <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-          Leaderboard
+        Leaderboard
       </h2>
-    <div class="flex flex-col h-full w-full block">
-      <div class="">
-        <div class="inline-block w-full overflow-hidden shadow sm:rounded-lg">
-          <table class="w-full h-full">
-            <thead class=" text-xs font-medium leading-4 tracking-wider text-left text-lime-600 uppercase border-b border-lime-950 bg-materialgreenlight dark:bg-materialgreenbg bg-cover">
+      <div class="flex flex-col h-full w-full block">
+        <div>
+          <div class="inline-block w-full overflow-hidden shadow sm:rounded-lg">
+            <table class="w-full h-full">
+              <thead class="text-xs font-medium leading-4 tracking-wider text-left text-lime-600 uppercase border-b border-lime-950 bg-materialgreenlight dark:bg-materialgreenbg bg-cover">
               <tr>
                 <th class="pl-20">
                   Player:
@@ -63,16 +65,14 @@ const getUserImageUrl = (imagePath) => {
                   Score
                 </th>
               </tr>
-            </thead>
-
-            <tbody class="bg-materialgreenlight dark:bg-materialgreenbg">
+              </thead>
+              <tbody class="bg-materialgreenlight dark:bg-materialgreenbg">
               <tr v-for="(u, index) in users" :key="index">
                 <td class="px-20 py-4 border-b border-lime-950 text-lime-600 bg-materialgreenlight dark:bg-materialgreenbg">
                   <div class="flex items-center">
-                    <div class="flex-shrink-0 w-30 h-30 ">
-                      <img  :src="getUserImageUrl(u.profile_photo_path)" alt="User Profile Photo" class="w-20 h-20 rounded-full  object-cover" />
+                    <div class="flex-shrink-0 w-30 h-30">
+                      <img :src="getUserImageUrl(u.profile_photo_path)" alt="User Profile Photo" class="w-20 h-20 rounded-full object-cover" />
                     </div>
-
                     <div class="ml-4">
                       <div class="text-[30px] font-medium leading-5 pl-6 text-lime-600">
                         {{ u.name }}
@@ -81,14 +81,14 @@ const getUserImageUrl = (imagePath) => {
                   </div>
                 </td>
                 <td class="px-6 py-4 text-[40px] leading-5 text-lime-600 border-b border-lime-950">
-                  {{u.score}}
+                  {{ u.score }}
                 </td>
               </tr>
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</AppLayout>
+  </AppLayout>
 </template>
